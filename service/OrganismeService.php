8f91bf7239ea63d9ac420ad50d20322e7285c932
Salah Eddine Chehbi <chehbi.se@gmail.com>
2018-10-04 17:15:26 +0100

    Add Function Create Organisme In service with contact

--- service/OrganismeService.php
+++ service/OrganismeService.php
@@ -9,7 +9,7 @@ class OrganismeService implements IDao {
     public function create($o) {
         $query = "INSERT INTO Organisme VALUES (NULL,?,?,?,?,?,?,?);";
         $req = $this->connexion->getConnexion()->prepare($query);
-	$req->execute(array($o->getNom(),$o->getTel(),$o->getFax(),$o->getMail(),$o->getSite(),$o->getContact,$o->getAdresse())) or die('Delete Error');
+	$req->execute(array($o->getNom(),$o->getTel(),$o->getFax(),$o->getMail(),$o->getSite(),$o->getContact(),$o->getAdresse())) or die('Delete Error');
     }
 
     public function delete($o) {
