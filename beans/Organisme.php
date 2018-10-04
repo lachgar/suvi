babcb274dacc501b46a212d993db1604aa717d8e
a <a@DESKTOP-70FPU48>
2018-10-04 16:13:12 +0100

    Bean Organisme

--- beans/Organisme.php
+++ beans/Organisme.php
@@ -1,16 +1,88 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- * Description of Organisme
- *
- * @author a
- */
 class Organisme {
-    //put your code here
+    private $id;
+    private $nom;
+    private $tel;
+    private $fax;
+    private $mail;
+    private $site;
+    private $contact;
+    private $adresse;
+    
+    function __construct($id, $nom, $tel, $fax, $mail, $site, $contact, $adresse) {
+        $this->id = $id;
+        $this->nom = $nom;
+        $this->tel = $tel;
+        $this->fax = $fax;
+        $this->mail = $mail;
+        $this->site = $site;
+        $this->contact = $contact;
+        $this->adresse = $adresse;
+    }
+    
+    function getId() {
+        return $this->id;
+    }
+
+    function getNom() {
+        return $this->nom;
+    }
+
+    function getTel() {
+        return $this->tel;
+    }
+
+    function getFax() {
+        return $this->fax;
+    }
+
+    function getMail() {
+        return $this->mail;
+    }
+
+    function getSite() {
+        return $this->site;
+    }
+
+    function getContact() {
+        return $this->contact;
+    }
+
+    function getAdresse() {
+        return $this->adresse;
+    }
+
+    function setId($id) {
+        $this->id = $id;
+    }
+
+    function setNom($nom) {
+        $this->nom = $nom;
+    }
+
+    function setTel($tel) {
+        $this->tel = $tel;
+    }
+
+    function setFax($fax) {
+        $this->fax = $fax;
+    }
+
+    function setMail($mail) {
+        $this->mail = $mail;
+    }
+
+    function setSite($site) {
+        $this->site = $site;
+    }
+
+    function setContact($contact) {
+        $this->contact = $contact;
+    }
+
+    function setAdresse($adresse) {
+        $this->adresse = $adresse;
+    }
+          
 }
