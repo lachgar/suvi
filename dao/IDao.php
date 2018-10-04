5b280584b2438ea1c2a3a61715e0fff5ba9c09d4
Salah Eddine Chehbi <chehbi.se@gmail.com>
2018-10-04 16:24:16 +0100

    Interface Idao

--- dao/IDao.php
+++ dao/IDao.php
@@ -2,4 +2,9 @@
 
 interface IDao {
     //put your code here
+    public function create($o);
+    public function delete($o);
+    public function update($o);
+    public function findById($id);
+    public function findAll();
 }
