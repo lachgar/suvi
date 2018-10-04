2e4108ef6d670e3905fe62711471b3ffe76a314c
Salah Eddine Chehbi <chehbi.se@gmail.com>
2018-10-04 17:03:43 +0100

    Add Function Create Organisme In

--- service/OrganismeService.php
+++ service/OrganismeService.php
@@ -16,7 +16,9 @@ include_once '../dao/IDao.php';
 class OrganismeService implements IDao {
     //put your code here
     public function create($o) {
-        
+        $query = "INSERT INTO Organisme VALUES (NULL,'".$o->getNom()."','".$o->getTel()."', '".$o->getFax()."','".$o->getMail()."','".$o->getSite()."','".$o->getContact()."','".$o->getAdresse()."');";
+        $req = $this->connexion->getConnexion()->prepare($query);
+	$req->execute() or die('Create Error');
     }
 
     public function delete($o) {
