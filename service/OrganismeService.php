d67c992b3f462811655d530fd0a0938223cf8bf7
a <a@DESKTOP-70FPU48>
2018-10-04 17:17:57 +0100

    Bean Organisme

--- service/OrganismeService.php
+++ service/OrganismeService.php
@@ -1,17 +1,17 @@
 <?php
 
-
 include_once '../dao/IDao.php';
 
 include_once '../connexion/Connexion.php';
 include_once '../beans/Organisme.php';
 
 class OrganismeService implements IDao {
+
     //put your code here
     public function create($o) {
-        $query = "INSERT INTO Organisme VALUES (NULL,?,?,?,?,?,?,?);";
+        $query = "INSERT INTO Organisme VALUES (NULL,?,?,?,?,?,?,?)";
         $req = $this->connexion->getConnexion()->prepare($query);
-	$req->execute(array($o->getNom(),$o->getTel(),$o->getFax(),$o->getMail(),$o->getSite(),$o->getContact(),$o->getAdresse())) or die('Delete Error');
+        $req->execute(array($o->getNom(), $o->getTel(), $o->getFax(), $o->getMail(), $o->getSite(), $o->getContact(), $o->getAdresse())) or die('Delete Error');
     }
 
     public function delete($o) {
